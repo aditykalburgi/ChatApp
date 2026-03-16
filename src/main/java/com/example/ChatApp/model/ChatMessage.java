f005b4b931bad5c343032028a5630c8e82710769
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-03-16 23:53:14 +0530

    added Chat logic

--- src/main/java/com/example/ChatApp/model/ChatMessage.java
+++ src/main/java/com/example/ChatApp/model/ChatMessage.java
@@ -1,4 +1,24 @@
 package com.example.ChatApp.model;
 
 public class ChatMessage {
+
+    private String sender;
+    private String content;
+
+
+    public String getSender(){
+        return sender;
+    }
+
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setConent(String content ){
+        this.content = content;
+    }
 }
