304a6e9009f41fe70be09a493de5186ee15fd358
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-03-16 23:48:00 +0530

    added webConfig code

--- src/main/java/com/example/ChatApp/Config/WebSocketConfig.java
+++ src/main/java/com/example/ChatApp/Config/WebSocketConfig.java
@@ -1,4 +1,33 @@
 package com.example.ChatApp.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-public class WebSocketConfig {
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer{
+
+    @Override
+
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+
+        config.enableSimpleBroker("/topic");
+
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/chat")
+         .setAllowedOriginPatterns("*")
+        .withSockJS();
+    }
+
+
+
+
+
+
+
 }
