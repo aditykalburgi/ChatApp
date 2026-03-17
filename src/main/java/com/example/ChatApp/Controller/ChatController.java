c11e8baade9969f5eeee4d7e1b08debfc6a4396d
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-03-17 11:25:23 +0530

    chat controller working logic

--- src/main/java/com/example/ChatApp/Controller/ChatController.java
+++ src/main/java/com/example/ChatApp/Controller/ChatController.java
@@ -1,4 +1,18 @@
 package com.example.ChatApp.Controller;
 
+import com.example.ChatApp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
 }
